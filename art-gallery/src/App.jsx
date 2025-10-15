1fa6e4e96d16231affbcdcdb9c05b4f8a257d8ef
Carolista <cjones.renaissancewoman@gmail.com>
2025-10-15 16:06:53 -0400

    removed part 2 solution code and added TODOs

--- art-gallery/src/App.jsx
+++ art-gallery/src/App.jsx
@@ -1,19 +1,45 @@
-import { useState } from 'react';
 import Header from './components/Header.jsx';
-import HomePage from './components/HomePage';
-import ArtworksPage from './components/ArtworksPage';
 import Footer from './components/Footer.jsx';
+import ArtworkCard from './components/ArtworkCard.jsx';
 import { mockArtworks } from '../src/test-data/mockArtworks.js';
 
+// TODO: Create HomePage and ArtworksPage
+
+// TODO: Move the artworksJSX code along with the entire <main> section below to
+//  ArtworksPage and render it there instead.
+
 function App() {
-    // State variables that React will pay attention to for re-rendering
-    const [currentPage, setCurrentPage] = useState('home');
+    // TODO: Add a state variable that will control which page is displaying
+
+    // TODO: Place an instance of HomePage and ArtworksPage where <main>
+    //  was before in the JSX returned below.
+
+    // TODO: Use the state variable with conditional rendering to display either
+    //  the home page or artworks page
+
+    // TODO: Pass the setter for the state variable to Header and HomePage so the
+    //  use can switch pages when interacting with child components
+
+    // TODO: Instead of accessing the artworks directly in the ArtworksPage
+    //  component, continue to import it here and pass the data down as a prop
+
+    let artworksJSX = [...mockArtworks].map((artwork) => {
+        return <ArtworkCard key={artwork.id} artwork={artwork} />;
+    });
 
     return (
         <div id="body-container">
-            <Header setCurrentPage={setCurrentPage} />
-            {currentPage === 'home' && <HomePage setCurrentPage={setCurrentPage} />}
-            {currentPage === 'artworks' && <ArtworksPage artworks={mockArtworks} />}
+            <Header />
+            <main className="main-content">
+                <h1>Artworks</h1>
+                {mockArtworks.length ? (
+                    <div className="artwork-card-container">{artworksJSX}</div>
+                ) : (
+                    <p>
+                        <em>We're sorry, there are no artworks to display at this time.</em>
+                    </p>
+                )}
+            </main>
             <Footer />
         </div>
     );
--- art-gallery/src/components/Header.jsx
+++ art-gallery/src/components/Header.jsx
@@ -1,12 +1,14 @@
-import NavMenu from './NavMenu';
+const Header = () => {
+    // TODO: Receive the setter prop from the parent where the state
+    //  variable lives
 
-const Header = ({ setCurrentPage }) => {
+    // TODO: Create a NavMenu component and pass along the setter for
+    //  changing the current page displayed from App.jsx
     return (
         <header>
             <div id="mag">
                 <strong>Midtown</strong> Art Gallery
             </div>
-            <NavMenu setCurrentPage={setCurrentPage} />
         </header>
     );
 };
