32141b9f59f390f825f18bcb95d74e0986829d66
Carolista <cjones.renaissancewoman@gmail.com>
2025-10-15 16:08:45 -0400

    removed part 2 TODOs

--- art-gallery/src/App.jsx
+++ art-gallery/src/App.jsx
@@ -3,26 +3,7 @@ import Footer from './components/Footer.jsx';
 import ArtworkCard from './components/ArtworkCard.jsx';
 import { mockArtworks } from '../src/test-data/mockArtworks.js';
 
-// TODO: Create HomePage and ArtworksPage
-
-// TODO: Move the artworksJSX code along with the entire <main> section below to
-//  ArtworksPage and render it there instead.
-
 function App() {
-    // TODO: Add a state variable that will control which page is displaying
-
-    // TODO: Place an instance of HomePage and ArtworksPage where <main>
-    //  was before in the JSX returned below.
-
-    // TODO: Use the state variable with conditional rendering to display either
-    //  the home page or artworks page
-
-    // TODO: Pass the setter for the state variable to Header and HomePage so the
-    //  use can switch pages when interacting with child components
-
-    // TODO: Instead of accessing the artworks directly in the ArtworksPage
-    //  component, continue to import it here and pass the data down as a prop
-
     let artworksJSX = [...mockArtworks].map((artwork) => {
         return <ArtworkCard key={artwork.id} artwork={artwork} />;
     });
--- art-gallery/src/components/Header.jsx
+++ art-gallery/src/components/Header.jsx
@@ -1,9 +1,4 @@
 const Header = () => {
-    // TODO: Receive the setter prop from the parent where the state
-    //  variable lives
-
-    // TODO: Create a NavMenu component and pass along the setter for
-    //  changing the current page displayed from App.jsx
     return (
         <header>
             <div id="mag">
