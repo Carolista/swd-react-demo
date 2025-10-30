d03f1793fcfd8cf8bca69a27192449eb4589749c
Carolista <cjones.renaissancewoman@gmail.com>
2025-10-30 00:19:42 -0400

    numbered the TODOs

--- art-gallery/src/App.jsx
+++ art-gallery/src/App.jsx
@@ -3,25 +3,25 @@ import Footer from './components/Footer.jsx';
 import ArtworkCard from './components/ArtworkCard.jsx';
 import { mockArtworks } from '../src/test-data/mockArtworks.js';
 
-// TODO: Create HomePage and ArtworksPage
+// TODO #1: Create new components, HomePage and ArtworksPage
 
-// TODO: Move the artworksJSX code along with the entire <main> section below to
-//  ArtworksPage and render it there instead.
+// TODO #2: Move the artworksJSX code along with the entire <main> section 
+//  below to ArtworksPage and render it there instead. Make sure ArtworkCard
+//  is imported in ArtworksPage instead of here. 
+//  Also, instead of accessing the artworks data directly in the ArtworksPage
+//  component, continue to import it here and pass the data down as a prop.
 
 function App() {
-    // TODO: Add a state variable that will control which page is displaying
+    // TODO #4: Add a state variable that will control which page is displaying.
 
-    // TODO: Place an instance of HomePage and ArtworksPage where <main>
+    // TODO #3: Place an instance of HomePage and ArtworksPage where <main>
     //  was before in the JSX returned below.
 
-    // TODO: Use the state variable with conditional rendering to display either
+    // TODO #5: Use the state variable with conditional rendering to display either
     //  the home page or artworks page
 
-    // TODO: Pass the setter for the state variable to Header and HomePage so the
+    // TODO #6: Pass the setter for the state variable to Header and HomePage so the
     //  use can switch pages when interacting with child components
-
-    // TODO: Instead of accessing the artworks directly in the ArtworksPage
-    //  component, continue to import it here and pass the data down as a prop
 
     let artworksJSX = [...mockArtworks].map((artwork) => {
         return <ArtworkCard key={artwork.id} artwork={artwork} />;
--- art-gallery/src/components/Header.jsx
+++ art-gallery/src/components/Header.jsx
@@ -1,8 +1,8 @@
 const Header = () => {
-    // TODO: Receive the setter prop from the parent where the state
+    // TODO #7: Receive the setter prop from the parent where the state
     //  variable lives
 
-    // TODO: Create a NavMenu component and pass along the setter for
+    // TODO #8: Create a NavMenu component and pass along the setter for
     //  changing the current page displayed from App.jsx
     return (
         <header>
