e6f3de5ffc3b0f889e9988cba8c774bf760f47f3
Carolista <cjones.renaissancewoman@gmail.com>
2026-07-10 18:18:21 -0400

    minor refactoring to improve heading hi erachy and other small updates

--- art-gallery/src/App.jsx
+++ art-gallery/src/App.jsx
@@ -1,13 +1,13 @@
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 import ArtworkCard from './components/ArtworkCard.jsx';
-import { mockArtworks } from '../src/test-data/mockArtworks.js';
+import { mockArtworks } from './test-data/mockArtworks.js';
 
-// TODO #1: Create new components, HomePage and ArtworksPage
+// TODO #1: Create new components, HomePage and ArtworksPage, then import them here.
 
-// TODO #2: Move the artworksJSX code along with the entire <main> section 
+// TODO #2: Move the artworksJSX code along with the entire <main> section
 //  below to ArtworksPage and render it there instead. Make sure ArtworkCard
-//  is imported in ArtworksPage instead of here. 
+//  is imported in ArtworksPage instead of here.
 //  Also, instead of accessing the artworks data directly in the ArtworksPage
 //  component, continue to import it here and pass the data down as a prop.
 
@@ -21,7 +21,7 @@ function App() {
     //  the home page or artworks page
 
     // TODO #6: Pass the setter for the state variable to Header and HomePage so the
-    //  use can switch pages when interacting with child components
+    //  user can switch pages when interacting with child components
 
     let artworksJSX = [...mockArtworks].map((artwork) => {
         return <ArtworkCard key={artwork.id} artwork={artwork} />;
--- art-gallery/src/components/Header.jsx
+++ art-gallery/src/components/Header.jsx
@@ -2,8 +2,8 @@ const Header = () => {
     // TODO #7: Receive the setter prop from the parent where the state
     //  variable lives
 
-    // TODO #8: Create a NavMenu component and pass along the setter for
-    //  changing the current page displayed from App.jsx
+    // TODO #8: Create and import a NavMenu component and pass along the 
+    // setter for changing the current page displayed from App.jsx
     return (
         <header>
             <div id="mag">
